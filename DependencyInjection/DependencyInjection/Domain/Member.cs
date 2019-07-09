246757a1c5a767c7ce120fa2880cf35c2c4c3b9e
İsmail KAŞAN <ismailkasan63@gmail.com>
2019-07-09 16:38:42 +0300

    Ornek Json kullanımı
    JSON kullanımı ile ilgili kodlar

---
+++ DependencyInjection/DependencyInjection/Domain/Member.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyInjection.Domain
+{
+    public class Member
+    {      
+        public string Name { get; set; }
+        public string LastName { get; set; }
+    }
+}
